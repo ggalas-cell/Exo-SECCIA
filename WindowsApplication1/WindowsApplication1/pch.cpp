caa26abed513cc64e70acd86bbbb9a123f793073
ggalas-cell <ggalas@gaming.tech>
2025-11-10 13:01:25 +0100

    feat : test

--- WindowsApplication1/WindowsApplication1/pch.cpp
+++ WindowsApplication1/WindowsApplication1/pch.cpp
@@ -1,1 +1,19 @@
-﻿#include "pch.h"
+﻿#include <windows.h>
+#include <filesystem>
+#include <iostream>
+#include "pch.h"
+
+
+int main()
+{
+	FILE* test;		
+	test = fopen("\Documents\GitHub\Exo - SECCIA\wolf.bmp", "rb");
+	if (test == nullptr)
+	{
+		return 0;
+	}
+	
+	std::cout << fopen;
+		fread;
+	return 0;
+}
---
+++ WindowsApplication1/test/test.cpp
@@ -0,0 +1,26 @@
+#include <windows.h>
+#include <filesystem>
+#include <iostream>
+
+int main()
+{
+	FILE* test;
+	
+	fopen_s(&test, "C:\\Users\\ggalas\\Documents\\GitHub\\Exo-SECCIA\\wolf .bmp", "rb");
+	if (test == nullptr)
+	{
+		return 0;
+	}
+
+//	std::cout << fopen;
+	BYTE* buf = new BYTE[5000000];
+	size_t size = fread(buf, 1, 5000000, test);
+	fclose(test);
+	BITMAPFILEHEADER head = {};
+	BITMAPINFOHEADER bih = {};
+	memcpy(test, buf, size);
+
+
+
+	return 0;
+}
