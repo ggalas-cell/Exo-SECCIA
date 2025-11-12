81a72afe51ba3f0e9ef4d7ba29e015eb837bb3c5
ggalas-cell <ggalas@gaming.tech>
2025-11-12 09:02:15 +0100

    feat : display

--- WindowsApplication1/test/test.cpp
+++ WindowsApplication1/test/test.cpp
@@ -12,13 +12,16 @@ int main()
 	}
 
 	//std::cout << fopen;
-	BYTE* buf = new BYTE[5000000];
-	size_t size = fread(buf, 1, 5000000, test);
+	BYTE* buf = new BYTE[5000000];	// (source)	//lecture du file -> buffer 
+	size_t size = fread(buf, 1, 5000000, test);		//idem
 	fclose(test);
-	BITMAPFILEHEADER head = {};
+	BITMAPFILEHEADER head = {};	//destination
 	BITMAPINFOHEADER bih = {};
-	memcpy(test, buf, size);
-
+	memcpy(&head, buf, sizeof(BITMAPFILEHEADER));
+	memcpy(&bih, buf+ sizeof(BITMAPFILEHEADER), sizeof(BITMAPINFOHEADER));
+	std::cout << (char)(head.bfType & 0xFF) << (char)(head.bfType >> 8) << "\n";
+	std::cout << "Taille :" << head.bfSize << "bytes\n";
+	std::cout << "offset des pixels :" << head.bfOffBits << "bytes\n";
 
 
 	return 0;
